0b7ef87036fef4cc00094c5b92624f7b92d6b11e
Christopher Rodriguez <mrmcpowned@users.noreply.github.com>
2020-06-26 01:55:59 -0400

    MVB: Minimum Viable Bot

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,13 @@
+import './config'
 import Discord from 'discord.js'
 
-const bot = new Discord.Client();
+const bot = new Discord.Client()
 
+bot.once('ready', () => {})
 
+bot.on('message', (msg) => {
+    if (msg.author.bot) return
+    if (msg.content == '!ping') msg.channel.send('pong!')
+})
 
+bot.login(process.env.BOT_TOKEN)
