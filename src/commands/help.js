19a268c26ca9a8b7e52ed675ece56b96320c61ce
Christopher Rodriguez <mrmcpowned@users.noreply.github.com>
2020-06-26 05:45:00 -0400

    Refactored command structure

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,12 +1,23 @@
+import { helpEmbed } from '../utils/helpEmbed'
+
+const name = 'Help' // User facing name of command
+const description = 'Provides help through several help articles' // User facing description
+const cname = ['h', 'help'] // Command name, or array of aliases
+const usage = '.h `articleName` `@Mention1` `@Mention2`...' // String showing usage
+const examples = ['.h cs <@702735514884898867>', '.h dualpc'] // String or Array of examples
+
 export default {
-    name: 'Help', // User facing name of command
-    description: 'Provides help through several help articles', // User facing description
-    cname: ['h', 'help'], // Command name, or array of aliases
-    usage: '.h `articleName` `@Mention1` `@Mention2`...', // String showing usage
-    examples: ['.h cs <@702735514884898867>', '.h dualpc'], // String or Array of examples
-    helpMessage: '', // Message that bot responds with when either no args are passed, or invoked via info command
+    name,
+    description,
+    cname,
+    usage,
+    examples,
+    helpMessage: helpEmbed({ name, description, cname, usage, examples }), // Message that bot responds with when either no args are passed, or invoked via info command
     execute: (msg, args) => {
         // The function executed by the command
-        msg.channel.send('Test')
+        if (!args.length) {
+            msg.client.commands.get('i').execute(msg, ['h'])
+            return
+        }
     },
 }
--- src/commands/link.js
+++ src/commands/link.js
@@ -1,10 +1,18 @@
+import { helpEmbed } from '../utils/helpEmbed'
+
+const name = 'Link' // User facing name of command
+const description = 'Sends a link to a specific page' // User facing description
+const cname = ['l', 'link'] // Command name, or array of aliases
+const usage = '.l `linkName` `@Mention1` `@Mention2`...' // String showing usage
+const examples = ['.l obs', '.l slobs <@702735514884898867>'] // String or Array of examples
+
 export default {
-    name: 'Link', // User facing name of command
-    description: 'Sends a link to a specific page', // User facing description
-    cname: ['l', 'link'], // Command name, or array of aliases
-    usage: '.l `linkName` `@Mention1` `@Mention2`...', // String showing usage
-    examples: ['.l obs', '.l slobs <@702735514884898867>'], // String or Array of examples
-    helpMessage: '', // Message that bot responds with when either no args are passed, or invoked via info command
+    name,
+    description,
+    cname,
+    usage,
+    examples,
+    helpMessage: helpEmbed({ name, description, cname, usage, examples }), // Message that bot responds with when either no args are passed, or invoked via info command
     execute: (msg, args) => {
         // The function executed by the command
     },
