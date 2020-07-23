7facaba5d76c1bb0deebc1370575f2b617440acd
Christopher Rodriguez <mrmcpowned@users.noreply.github.com>
2020-07-22 20:57:14 -0400

    Added custom prefix env

--- src/bot.js
+++ src/bot.js
@@ -36,7 +36,17 @@ for (const file of commandFiles) {
 bot.uniqueCommands = [...new Set(bot.commands.array())]
 
 bot.on('message', (msg) => {
-    const commandSyntax = new RegExp(/^\.(\w+)(?:\ (\S.*)|)$/i)
+    const escPrefix = process.env.PREFIX.split('')
+        .map((c) => `\\${c}`)
+        .join('')
+
+    console.log(prefix)
+
+    const regexStr = `^${escPrefix}(\\w+)(?:\\ (\\S.*)|)$`
+
+    console.log('Regex: ', regexStr)
+
+    const commandSyntax = new RegExp(regexStr, 'i')
     if (msg.author.bot) return
     if (!msg.content.match(commandSyntax)) return
 
