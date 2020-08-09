997c36b9735a5c39c9f24708ffa266e6c8564cd8
Christopher Rodriguez <mrmcpowned@users.noreply.github.com>
2020-08-09 02:26:37 -0400

    Used template string for welcome message

---
+++ src/data/templates.js
@@ -0,0 +1,10 @@
+export const welcomeTemplate = `:wave_tone2: Welcome to the **__GamePad Viewer Discord__** {userMention}! <:gamepad:230123446972055552>
+
+Feel free to stick around and chat with people in {lobbyChannel}, and if you're still curious about what else there is to do, here's a guide
+> - **Interested in getting new skins or making your own?** {skinsChannel} is the channel you're looking for!
+> - **Need help with GPV?** Feel free to ask your question in {supportChannel} and someone will try to help you as best as they can.
+> - **Want to know when GPV 2.0 launches?** You can now **__follow__** {updatesChannel} to get info about GPV straight to your server!
+> - **Curious about what other things this bot can do?** You can run the command \`{botCommand}\` in the {botChannel} channel to learn more.
+
+<:patreon:741878715109212210> **__Want to support the development of GamePad Viewer?__**
+You can become a patron of GPV today by visiting <https://patreon.com/gpv>!`
