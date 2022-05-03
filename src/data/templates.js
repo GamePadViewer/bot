bb06d117b1acd92e9e196f202e5894ce4fcdff8c
Christopher Rodriguez <mrmcpowned@users.noreply.github.com>
2022-05-03 01:28:09 -0400

    Update templates.js

--- src/data/templates.js
+++ src/data/templates.js
@@ -9,10 +9,13 @@ Feel free to stick around and chat with people in {lobbyChannel}, and if you're 
 {endMessage}`
 
 export const endMessages = [
+    `🎨 **__Do you make custom skins or know somebody who does?__**
+    GPV is looking to get feedback from skin creators! If you're a skin creator, feel free fill out the short (6 questions) feedback form. If you know someone who creates skins, send them the form too! <https://forms.gle/bm4SQgkJEp16LyNR7>`,
+]
+
+export const oldEndMessages = [
     `<:patreon:741878715109212210> **__Want to support the development of GamePad Viewer?__**
     You can become a patron of GPV today by visiting <https://patreon.com/gpv>!`,
     `📢 **__Have some feedback about GamePad Viewer?__**
     If you've got feedback, please feel free to fill out the feedback form below. If you choose to be contacted in the future, you might even be able to try out GPV 2.0 before everyone else! <https://forms.gle/BEebQWpbiSktnbHk9>`,
-    `🎨 **__Do you make custom skins or know somebody who does?__**
-    GPV is looking to get feedback from skin creators! If you're a skin creator, feel free fill out the short (6 questions) feedback form. If you know someone who creates skins, send them the form too! <https://forms.gle/bm4SQgkJEp16LyNR7>`,
 ]
