9070613a6f58a6850a6a44c638094d236b958091
Christopher Rodriguez <mrmcpowned@users.noreply.github.com>
2022-09-30 00:41:15 -0400

    Update welcome message (#23)
    * Updated welcome message
    
    * Update template
    
    * Extra updates to welcome message

--- src/data/templates.js
+++ src/data/templates.js
@@ -1,19 +1,25 @@
-export const welcomeTemplate = `:wave_tone2: Welcome to the **__GamePad Viewer Discord__** {userMention}! <:gamepad:230123446972055552>
+export const welcomeTemplate = `:wave_tone2: Hi {userMention}!
 
-Feel free to stick around and chat with people in {lobbyChannel}, and if you're still curious about what else there is to do, here's a guide
-> - **Interested in getting new skins or making your own?** {skinsChannel} is the channel you're looking for!
-> - **Need help with GPV?** Feel free to ask your question in {supportChannel} and someone will try to help you as best as they can.
-> - **Want to know when GPV 2.0 launches?** You can now **__follow__** {updatesChannel} to get info about GPV straight to your server!
-> - **Curious about what other things this bot can do?** You can run the command \`{botCommand}\` in the {botChannel} channel to learn more.
+<:gamepad:230123446972055552> Welcome to the **__GamePad Viewer Discord__** <:gpvg:949928599677444136><:gpvp:949928599400616017><:gpvv:949928599790698577>
+Here's a getting started list you can follow while you're here: 
+> **{introChannel}** - Introduce yourself to the server 🗣
+> **{rolesChannel}** - Tag yourself with roles you're interested in 🏷
+> **{lobbyChannel}** - Chat about anything offtopic 👨‍💻
+> **{skinsChannel}** - Discuss skins you like or get help making a new one 🖌
+> **{supportChannel}** - Get support regarding any GPV related issues you might be having 🛠
+> **{feedbackChannel}** - Give some feedback regarding the GPV tool, the GPV Discord, or anything else GPV related ✍
+> **{updatesChannel}** - Follow this channel to get updates about GPV in your own Discord server 📣
 
 {endMessage}`
 
 export const endMessages = [
-    `🗣 **__First time here? Let us know what you think about the Discord!__**
-    We're looking for some quick feedback (5 questions) regarding the Discord! If you know someone who's been in the server for a while, feel free to send them the form too <https://forms.gle/qhKAKSdxnMQwcR2e8>`,
+    `⁉ **__Did you know you get perks for boosting this server?__** <:discord:1024569352210624552>
+    If you choose to be a **Server Booster** you'll get early access to beta testing GPV 2.0, the ability to promote your stream in {promoChannel}, and other future perks! If you have Discord Nitro already, **you already have 2 free boosts** per month you can use, so make sure to follow this guide to get started boosting today: <https://support.discord.com/hc/en-us/articles/360028038352-Server-Boosting-FAQ-#h_01GCD8SSZ6HN132NAW16G4523G>`,
 ]
 
 export const oldEndMessages = [
+    `🗣 **__First time here? Let us know what you think about the Discord!__**
+    We're looking for some quick feedback (5 questions) regarding the Discord! If you know someone who's been in the server for a while, feel free to send them the form too <https://forms.gle/qhKAKSdxnMQwcR2e8>`,
     `<:patreon:741878715109212210> **__Want to support the development of GamePad Viewer?__**
     You can become a patron of GPV today by visiting <https://patreon.com/gpv>!`,
     `📢 **__Have some feedback about GamePad Viewer?__**
